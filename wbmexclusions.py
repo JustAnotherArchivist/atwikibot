59f9e666df3e5f7180b87bf5893d3f7de99a050f
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2019-03-08 18:46:50 +0100

    WBM exclusion list

---
+++ wbmexclusions.py
@@ -0,0 +1,52 @@
+import collections
+import mwclient
+import re
+import os
+
+
+extractionPattern = re.compile('[^:/]+://(?:www\.)?([^/]+)')
+
+
+def get_cleaned_domain(line):
+	# Extract the domain from the first URL appearing on a line, stripping away a leading "www." if any; returns None if no URL is found
+	match = extractionPattern.search(line)
+	if match:
+		return match.group(1)
+	return None
+
+
+def main():
+	site = mwclient.Site('archiveteam.org', path = '/')
+
+	page = site.Pages['List of websites excluded from the Wayback Machine']
+
+	# Extract domains from lines
+	entries = collections.deque((line, get_cleaned_domain(line)) for line in page.text().split('\n'))
+
+	# Identify blocks of URLs and sort them
+	entries.append((None, None)) # Dummy entry at the end to trigger a last sorting if necessary
+	output = []
+	currentBlock = []
+	while entries:
+		line, domain = entries.popleft()
+		if domain is None:
+			# Either a line without a URL or the dummy entry at the end
+			if currentBlock:
+				currentBlock.sort(key = lambda x: x[1])
+				output.extend(x[0] for x in currentBlock)
+				currentBlock = []
+			if line is not None: # Ignore the dummy entry
+				output.append(line)
+		elif line is not None:
+			# line and domain are not None, i.e. this is a line with a URL in it
+			currentBlock.append((line, domain))
+
+	outputStr = '\n'.join(output)
+
+	# Update if necessary
+	if page.text() != outputStr:
+		site.login(os.environ['ATWIKIBOT_USERNAME'], os.environ['ATWIKIBOT_PASSWORD']) # Only log in when necessary
+		page.save(outputStr)
+
+
+main()
